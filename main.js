ffe8cd63a78cc3fe789fbf440a158eac0d9b0377
hsoubecommerce <hsoubecommerce@gmail.com>
2023-01-01 12:50:24 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,18 @@
+let myName = "My name : BDOUR";
+
+let index = 1;
+
+function writeText (){
+    document.querySelector ('h1').textContent = myName.slice (0, index)
+
+    index ++
+
+    if(index > myName.length){
+        index = 1 ;
+    }
+}
+ setInterval (function (){
+    writeText ();
+ }, 200)
+
+
